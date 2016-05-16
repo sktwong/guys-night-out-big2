07428b9475f37678c4abde6eecf8c5efa30a9f01
sktwong <sktwong@gmail.com>
2016-05-15 22:16:49 -0400

    Added Angular support, enabled player name editing

--- big2/js/app.js
+++ big2/js/app.js
@@ -2,14 +2,55 @@
  * Guy's Night Out
  * Big 2 - Score Calculator
  */
-$(document).ready(function() {
+ var big2App = angular.module('big2App',[]);
+ big2App.controller('Big2Controller', ['$scope', Big2ControllerFn]);
 
-    // Enable modal click on rows
-    $('tr.players').on('click touch', function() {
-        $('#edit-players').modal('show');
-    });
+ function Big2ControllerFn ($scope) {
 
-    $('tr.scores').on('click touch', function() {
+    // Available functions
+    $scope.editScores = editScores;
+    $scope.editPlayers = editPlayers;
+    $scope.savePlayers = savePlayers;
+
+    // Data objects
+    $scope.players = {}; // Player names
+
+    function editScores() {
         $('#edit-scores').modal('show');
-    });
-});
+    }
+
+    // Displays edit players modal
+    function editPlayers() {
+        // Set player names if present
+        $('#player1').val($scope.players.one);
+        $('#player2').val($scope.players.two);
+        $('#player3').val($scope.players.three);
+        $('#player4').val($scope.players.four);
+
+        // Show modal
+        $('#edit-players').modal('show');
+
+        // Auto focus first field (doesn't work on iOS)
+        // $('#edit-players').on('shown.bs.modal', function () {
+        //     $('#player1').focus();
+        // })  
+
+        // Allow Enter key to save players
+        $('#edit-players').keypress(function(e) {
+            console.log('enter');
+            if(e.which == 13) {
+                $('#save-players').click();
+            }
+        });
+    }
+
+    // Saves player names to scope, then closes modal
+    function savePlayers() {
+        $scope.players.one = $('#player1').val();
+        $scope.players.two = $('#player2').val();
+        $scope.players.three = $('#player3').val();
+        $scope.players.four = $('#player4').val();
+
+        $('#edit-players').modal('hide');
+    }
+}
