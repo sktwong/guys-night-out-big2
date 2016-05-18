fe9f76336c399170d0ad12713cd13ccee11af9d7
sktwong <sktwong@gmail.com>
2016-05-17 23:28:18 -0400

    Removing sample prostitute selection script

--- guys_night_v2.py
+++
@@ -1,4 +0,0 @@
-# guys_night_v2.py #
-kelvin_free_on_21 = True
-guys_night = 21 if kelvin_free_on_21 else 20
-print "\nGuy's night is on May {}. Be there or be square. Steve's turn to hire the prostitute.\n".format(guys_night)
