106b8b1351b8d08313acf1166923e80338fa6bd6
sktwong <sktwong@gmail.com>
2016-07-15 00:31:16 -0400

    Ported over document listeners to navbar directive

--- src/styles.js
+++ src/styles.js
@@ -5,26 +5,26 @@
  * General page styling scripts
  *
  */
-$(document).ready(function() {
-    var href = window.location.href;
+// $(document).ready(function() {
+//     var href = window.location.href;
 
-    // Closes the navbar menu on click
-    $(document).on('click', '.navbar-collapse.in', function(e) {
-        if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-            $(this).collapse('hide');
-        }
-    });
+//     // Closes the navbar menu on click
+//     $(document).on('click', '.navbar-collapse.in', function(e) {
+//         if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+//             $(this).collapse('hide');
+//         }
+//     });
 
-    // Sets active navbar class
-    if (href.indexOf('data') > 0) {
-        $('.nav li').eq(1).addClass('active');
-    } else {
-        $('.nav li').eq(0).addClass('active');
-    }
+//     // Sets active navbar class
+//     if (href.indexOf('data') > 0) {
+//         $('.nav li').eq(1).addClass('active');
+//     } else {
+//         $('.nav li').eq(0).addClass('active');
+//     }
 
-    // Updates active navbar item
-    $('.nav a').on('click', function() {
-        $('.nav').find('.active').removeClass('active');
-        $(this).parent().addClass('active');
-    });
-});
+//     // Updates active navbar item
+//     $('.nav a').on('click', function() {
+//         $('.nav').find('.active').removeClass('active');
+//         $(this).parent().addClass('active');
+//     });
+// });
